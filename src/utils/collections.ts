4c0fe5471352456c467aedbe2ba0bdfa67900e1c
Athena Freitas <athenacfr@gmail.com>
2025-02-05 09:02:49 -0400

    feat: refactor collection mapping

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -2,6 +2,7 @@ import {
   type CollectionEntry,
   type CollectionKey,
   type DataEntryMap,
+  getCollection,
   getEntry,
   render,
 } from 'astro:content';
@@ -23,17 +24,44 @@ export const renderEntry = async <
   return Content;
 };
 
-export const parseEntry = (entry: CollectionEntry<CollectionKey>) => ({
-  ...entry,
-  name: entry.id.split('/').splice(-1).join('/'),
-  subpath: entry.id.split('/').slice(0, -1).join('/'),
-});
+export type ExtendedCollection = ReturnType<typeof extendCollection>;
+export type ExtendedCollectionEntry = ExtendedCollection[number];
 
-export const getCollectionSubpaths = (
-  entries: CollectionEntry<CollectionKey>[],
-) =>
-  R.pipe(
+export const extendCollection = <C extends keyof DataEntryMap>(
+  entries: CollectionEntry<C>[],
+) => {
+  return R.pipe(
     entries,
-    R.map((entry) => parseEntry(entry).subpath),
-    R.unique(),
+    R.flatMap((post) => {
+      const path = post.id;
+
+      const subpaths = path
+        .split('/')
+        .map((_, index, array) => array.slice(0, index).join('/'));
+
+      return [
+        ...(path !== 'index' ? [{ path }] : []),
+        ...subpaths.map((subpath) => ({ path: subpath })),
+      ];
+    }),
+    R.uniqueBy((entry) => entry.path),
+    R.map((entry) => {
+      const post = entries.find((post) => post.id === (entry.path || 'index'));
+      const name = entry.path.split('/').slice(-1).join('/');
+
+      return { ...entry, name, post };
+    }),
+    R.sort((entry) => (entry.post?.data.date ? -entry.post.data.date : 0)),
+    (array) =>
+      R.map(array, (entry) => {
+        const related = array.filter((currentEntry) => {
+          const dir = `/${currentEntry.path}`.split('/').at(-2);
+
+          return dir === entry.path && currentEntry.path !== entry.path;
+        });
+
+        return { ...entry, related };
+      }),
+    // R.sort((entry) => (entry.related ? 1 : -1)),
   );
+};
